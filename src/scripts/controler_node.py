2c991c3aef7ca53044f5f9b36596559b02cc1850
KhaledMontaser2024 <khaledmontaser2024@alexu.edu.eg>
2023-10-29 11:32:14 +0200

    datatype minimization

--- src/scripts/controler_node.py
+++ src/scripts/controler_node.py
@@ -5,8 +5,6 @@
 ## to match our needs then creating a ROS node to periodically send commands to the main ##
 ## system node                                                                           ##
 ###########################################################################################
-
-
 import pygame
 import rospy
 import json
@@ -19,20 +17,19 @@ class controller():
         pygame.init()
         self.PS_Controller = pygame.joystick.Joystick(0)
         self.PS_Controller.init()
-        
         ##############################################
 
         rospy.init_node("Controller_Node")
         command_publisher=rospy.Publisher("/Controller_to_caiman",ButtonsStateMessage,queue_size=10)
 
         ########### json file that contains ps keys to easily manipulate them ######## 
-        file = open('/home/montaser/catkin_ws/src/rov_system/src/scripts/ps_keys.json', 'r+')
+        file = open('src/scripts/ps_keys.json', 'r+')
         self.keys = json.load(file)
         ##############################################################################
 
 
         self.ButtonsState = ButtonsStateMessage() ## custom ros msg contains all ps keys 
-        self.ButtonsState.motion.L1_R1=50
+        self.ButtonsState.MotionData.Speed=50
         
         #### to use later in L2 R2 axis combination ######
         self.L2=float()
@@ -43,8 +40,6 @@ class controller():
         while not rospy.is_shutdown():
             self.event_type_check()
             command_publisher.publish(self.ButtonsState)
-            print(self.ButtonsState.motion)
-            
         ################################################
 
         
@@ -59,51 +54,58 @@ class controller():
                 self.Analogs(event)
     #######################################################################################
     
-    ############# this fn toggles the state of each button when pressed ###################
+    ######################## this fn toggles the state of each button when pressed ##########################
     def Buttons(self,event):
-        self.ButtonsState.x        ^= event.button == self.keys['x']
-        self.ButtonsState.square   ^= event.button == self.keys['square']
-        self.ButtonsState.triangle ^= event.button == self.keys['triangle']
-        self.ButtonsState.circle   ^= event.button == self.keys['circle']
+        ################# in 4 bits, 4 buttons are toggled when pressed and sent to arduino #################
+        self.ButtonsState.ArduinoData ^= ((event.button == self.keys['x'])        << 0)  
+        self.ButtonsState.ArduinoData ^= ((event.button == self.keys['square'])   << 1)  
+        self.ButtonsState.ArduinoData ^= ((event.button == self.keys['triangle']) << 2) 
+        self.ButtonsState.ArduinoData ^= ((event.button == self.keys['circle'])   << 3) 
+        #####################################################################################################
         
-        self.ButtonsState.PS      ^= event.button == self.keys['PS']
-        self.ButtonsState.share   ^= event.button == self.keys['share']
-        self.ButtonsState.options ^= event.button == self.keys['options']
-        
-        self.ButtonsState.L3 ^= event.button == self.keys['L3']
-        self.ButtonsState.R3 ^= event.button == self.keys['R3']
-       
+        ################# in 4 bits, 4 buttons are toggled when pressed and sent to Caiman #################
+        self.ButtonsState.CommunicationData ^= ((event.button == self.keys['share'])   << 0) 
+        self.ButtonsState.CommunicationData ^= ((event.button == self.keys['options']) << 1) 
+        self.ButtonsState.CommunicationData ^= ((event.button == self.keys['L3'])      << 2)
+        self.ButtonsState.CommunicationData ^= ((event.button == self.keys['R3'])      << 3)
+        #####################################################################################################
+
         ######################### R1 and L1 use one variable to change speed taking range from 50 to 200 ##################
-        self.ButtonsState.motion.L1_R1 -= 50 * ((event.button == self.keys['L1']) and  (self.ButtonsState.motion.L1_R1>50))
-        self.ButtonsState.motion.L1_R1 += 50 * ((event.button == self.keys['R1']) and (self.ButtonsState.motion.L1_R1<200))
+        self.ButtonsState.MotionData.Speed -= 50 * ((event.button == self.keys['L1']) and (self.ButtonsState.MotionData.Speed>50))
+        self.ButtonsState.MotionData.Speed += 50 * ((event.button == self.keys['R1']) and (self.ButtonsState.MotionData.Speed<200))
+        ###################################################################################################################
     
-    ########################################################################################
+        self.ButtonsState.MotionData.Reverse_Control ^= event.button == self.keys['PS'] 
+    
+    ########################################################################################################################
 
 
-    ############ this fn toggles the state of each Arrow when pressed ######################
-    def Arrows(self,event):
-        self.ButtonsState.UpArrow    ^= event.value[1] == self.keys['UpArrow']
-        self.ButtonsState.DownArrow  ^= event.value[1] == self.keys['DownArrow']
-        self.ButtonsState.LeftArrow  ^= event.value[0] == self.keys['LeftArrow']
-        self.ButtonsState.RightArrow ^= event.value[0] == self.keys['RightArrow']
+    ############ this fn toggles the state of each Arrow when pressed ##########################
+    def Arrows(self,event): 
+        ##in Communication higher nibble arrows are stored and sent to caiman ## Modify for further use 
+        self.ButtonsState.CommunicationData ^= ((event.value[1] == self.keys['UpArrow'])    << 4)
+        self.ButtonsState.CommunicationData ^= ((event.value[1] == self.keys['DownArrow'])  << 5)  
+        self.ButtonsState.CommunicationData ^= ((event.value[0] == self.keys['LeftArrow'])  << 6) 
+        self.ButtonsState.CommunicationData ^= ((event.value[0] == self.keys['RightArrow']) << 7) 
     #########################################################################################
-
+    
 
     def Analogs(self,event):
-        
+
+
         ################# R2 and L2 axises are combined into one axis ####################### 
         if event.axis == self.keys['R2']:
             self.R2 = round(event.value + 1 , 4)
         if event.axis == self.keys['L2']:
             self.L2 = round(event.value + 1 , 4) 
-        self.ButtonsState.motion.R2_L2_Axis = self.R2 - self.L2
+        self.ButtonsState.MotionData.Forward_Backward_Axis = self.R2 - self.L2
         ######################################################################################
 
         ############### right and left analogs manipulation ##################################
-        self.ButtonsState.motion.RightAnalog_Up_Down_Axis    =  round(-self.PS_Controller.get_axis(self.keys['RightAnalog_Up_Down_Axis']),4)
-        self.ButtonsState.motion.RightAnalog_Left_Right_Axis =  round( self.PS_Controller.get_axis(self.keys['RightAnalog_Left_Right_Axis']),4)
-        self.ButtonsState.motion.LeftAnalog_Up_Down_Axis     =  round(-self.PS_Controller.get_axis(self.keys['LeftAnalog_Up_Down_Axis']),4)
-        self.ButtonsState.motion.LeftAnalog_Left_Right_Axis  =  round( self.PS_Controller.get_axis(self.keys['LeftAnalog_Left_Right_Axis']),4)
+        self.ButtonsState.MotionData.Pitch_Axis       =  round(-self.PS_Controller.get_axis(self.keys['RightAnalog_Up_Down_Axis']),4)
+        self.ButtonsState.MotionData.Yaw_Axis         =  round( self.PS_Controller.get_axis(self.keys['RightAnalog_Left_Right_Axis']),4)
+        self.ButtonsState.MotionData.Up_Down_Axis     =  round(-self.PS_Controller.get_axis(self.keys['LeftAnalog_Up_Down_Axis']),4)
+        self.ButtonsState.MotionData.Left_Right_Axis  =  round( self.PS_Controller.get_axis(self.keys['LeftAnalog_Left_Right_Axis']),4)
         #######################################################################################
 
 
