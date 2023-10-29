7b7c91d11a8b3f900bc6da5b32388f9101ec6d6e
YoussefAbdelsamea <142221678+YoussefAbdelsamea@users.noreply.github.com>
2023-10-29 21:24:51 +0200

    adding method to check the speed on thrusters and testing everything

--- ROV_motion.py
+++ ROV_motion.py
@@ -20,6 +20,8 @@ class motion:
         self.BFL = 0
         self.BBR = 0
         self.BBL = 0
+        
+        self.thrusters = [None] * 8
     
     def motion_eqn(self):
         self.TFR = (( self.forward_backward / 2) - self.slide - self.up_down + self.roll - self.yaw + self.pitch) * self.max_speed
@@ -31,11 +33,37 @@ class motion:
         self.BFL = ((-self.forward_backward / 2) + self.slide + self.up_down + self.roll + self.yaw - self.pitch) * self.max_speed
         self.BBR = (( self.forward_backward / 2) - self.slide + self.up_down - self.roll + self.yaw + self.pitch) * self.max_speed
         self.BBL = (( self.forward_backward / 2) + self.slide + self.up_down + self.roll - self.yaw + self.pitch) * self.max_speed
+        
+        self.thrusters = [self.TFR, self.TFL, self.TBR, self.TBL, self.BFR, self.BFL, self.BBR, self.BBL]
+    
+    def add_PID(self):
+        x = 0
+    
+    def check_speed(self):
+        max_val = max(map(abs, self.thrusters))
+        scale = 1
+        
+        if max_val > self.max_speed:
+            scale = self.max_speed / max_val
+        
+        self.thrusters = [element * scale for element in self.thrusters]
     
     def output(self):
-        
         self.motion_eqn
+        #self.add_PID()
+        self.check_speed()          
+
+        return self.thrusters
 
 if __name__ == "__main__":
     
     ROV= motion(2, 1, 1, 0, 0, 0, 200, 0)
+    print(ROV.output())
+    ROV= motion(2, 1, 1, 0.6, -1, 1, 200, 0)
+    print(ROV.output())
+    ROV= motion(2, 1, 1, 0.8, -0.3, 0, 200, 0)
+    print(ROV.output())
+    ROV= motion(2, -1, 1, 0, 0, -0.6, 200, 0)
+    print(ROV.output())
+    ROV= motion(2, 1, 1, 0.5, -0.8, 0, 200, 0)
+    print(ROV.output())
