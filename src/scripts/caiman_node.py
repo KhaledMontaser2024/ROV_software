f0a4960d12fb7251b6b8988323be2e0f6f0cc301
3ela2 <96801945+3ela2@users.noreply.github.com>
2023-11-02 02:02:45 +0200

    Update caiman_node.py
    Made an instance of  motion class and got its output

--- src/scripts/caiman_node.py
+++ src/scripts/caiman_node.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 import rospy
 from rov_system.msg import ButtonsStateMessage
-
+from ROV_motion import motion
 
 class Caiman():
     def __init__(self) -> None:
@@ -12,5 +12,8 @@ class Caiman():
 
     def commandsHandler(self, msg):
         rospy.loginfo(msg.motion)
+        motion= motion(msg.MotionData.Forward_Backward_Axis, msg.MotionData.Up_Down_Axis, msg.ButtonsState.MotionData.Left_Right_Axis, msg.MotionData.Yaw_Axis, msg.MotionData.Pitch_Axis, roll, 200, msg.MotionData.Reverse_Control)
+        thrusters_speeds=motion.output()
+        
 
 Caiman()
